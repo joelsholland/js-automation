71ef46d95a966b198bbb0fbeb6b9577bd40f60cd
Joel Trepp <joeltrepp@hotmail.com>
2023-08-15 19:09:49 +0200

    add

---
+++ power-automate/powerAutomateExpression.js
@@ -0,0 +1,19 @@
+function generatePA() {
+        var action = document.getElementById('t1').value.trim().replace(/\s+/,'_');
+        var element = document.getElementById('t2').value.trim();
+
+        console.log('action:', action)
+        console.log('element:', element)
+
+        //if 'element' field is empty 
+        if (element.length === 0) {
+                var exp = `first(body('${action}')?['value'])`;
+        }
+        else if (element.length > 0) {
+                var exp = `first(body('${action}')?['value'])?['${element}']`;
+
+        }
+
+        document.getElementById('response').value = exp;
+
+}
