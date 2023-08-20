f598e12214f10c48d21195ebc6d3f5513f2e5484
Joel Trepp <joeltrepp@hotmail.com>
2023-08-20 20:41:03 +0200

    colors

--- power-automate/powerAutomateExpression.js
+++ power-automate/powerAutomateExpression.js
@@ -1,19 +1,32 @@
 function generatePA() {
-        var action = document.getElementById('t1').value.trim().replaceAll(/\s+/g,'_');
-        var element = document.getElementById('t2').value.trim();
+        const action = document.getElementById('t1').value.trim().replaceAll(/\s+/g, '_');
+        const element = document.getElementById('t2').value.trim();
+        const response = document.getElementById('response');
+        const beforeAction = "first(body('";
+        const afterAction = "')?['value'])";
 
-        console.log('action:', action)
-        console.log('element:', element)
+        //clear whatever was in the output
+        response.innerHTML = ''
 
-        //if 'element' field is empty 
-        if (element.length === 0) {
-                var exp = `first(body('${action}')?['value'])`;
+        //create colorful span
+        actionSpan = document.createElement("span")
+        actionSpan.setAttribute("class", "actionColor")
+        actionSpan.innerText = action
+
+        response.append(beforeAction, actionSpan, afterAction);
+
+        if (element.length > 0) {
+                beforeElement = "?['";
+                afterElement = "']";
+
+                elementSpan = document.createElement("span");
+                elementSpan.setAttribute("class", "elementColor");
+                elementSpan.innerText = element;
+
+                response.append(beforeElement, elementSpan, afterElement);
+
         }
-        else if (element.length > 0) {
-                var exp = `first(body('${action}')?['value'])?['${element}']`;
 
-        }
 
-        document.getElementById('response').value = exp;
 
 }
