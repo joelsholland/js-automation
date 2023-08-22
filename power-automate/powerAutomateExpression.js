041f03e1ffb855b5c5b4a413aac0f57d244e5b46
Joel Trepp <joeltrepp@hotmail.com>
2023-08-22 20:02:43 +0200

    slick event listener

--- power-automate/powerAutomateExpression.js
+++ power-automate/powerAutomateExpression.js
@@ -1,28 +1,38 @@
-function generatePA() {
-        const action = document.getElementById('t1').value.trim().replaceAll(/\s+/g, '_');
-        const element = document.getElementById('t2').value.trim();
+function powerAutomate() {
+        const actionField = document.getElementById('t1');
+        const elementField = document.getElementById('t2');
         const response = document.getElementById('response');
-        const beforeAction = "first(body('";
-        const afterAction = "')?['value'])";
-        const beforeElement = "?['";
-        const afterElement = "']";
-        
-        //clear whatever was in the output
-        response.innerHTML = ''
 
-        //create colorful span
-        actionSpan = document.createElement("span")
-        actionSpan.setAttribute("class", "actionColor")
-        actionSpan.innerText = action
+        actionField.addEventListener("input", generatePA);
+        elementField.addEventListener("input", generatePA);
 
-        response.append(beforeAction, actionSpan, afterAction);
+        function generatePA() {
+                const action = document.getElementById('t1').value.trim().replaceAll(/\s+/g, '_');
+                const element = document.getElementById('t2').value.trim();
+                const beforeAction = "first(body('";
+                const afterAction = "')?['value'])";
 
-        if (element.length > 0) {
+                //clear whatever was in the output
+                response.innerHTML = ''
 
-                elementSpan = document.createElement("span");
-                elementSpan.setAttribute("class", "elementColor");
-                elementSpan.innerText = element;
+                //create colorful span
+                actionSpan = document.createElement("span")
+                actionSpan.setAttribute("class", "actionColor")
+                actionSpan.innerText = action
 
-                response.append(beforeElement, elementSpan, afterElement);
+                //show output with only action
+                response.append(beforeAction, actionSpan, afterAction);
+
+                if (element.length > 0) {
+                        beforeElement = "?['";
+                        afterElement = "']";
+
+                        elementSpan = document.createElement("span");
+                        elementSpan.setAttribute("class", "elementColor");
+                        elementSpan.innerText = element;
+                        
+                        //show output with action and element
+                        response.append(beforeElement, elementSpan, afterElement);
+                }
         }
 }
