8cd165ba49a9eb0e29469df583f5b3f5f5f4359d
Joel Trepp <joeltrepp@hotmail.com>
2023-09-03 15:49:44 +0200

    checkbox logic

--- fetchxml/fetchxml.js
+++ fetchxml/fetchxml.js
@@ -1,45 +1,44 @@
+const colField = document.querySelector('.inputCol');
+const input = document.querySelector('.inputarea');
+const btn = document.querySelector('button');
+const checkbox = document.querySelector('.filterCheckbox');
+const output = document.querySelector('.outputarea');
 
+input.addEventListener("input",generateFetch);
+checkbox.addEventListener("click",generateFetch);
 
-function generateFetchXML() {
-
-  const colField = document.querySelector('.inputCol');
-  const input = document.querySelector('.inputarea');
-  const btn = document.querySelector('button');
-  const checkbox = document.querySelector('.filterCheckbox');
-  const output = document.querySelector('.outputarea');
-  
+function generateFetch(filterOption) {
   //set focus on column field
-  colField.focus()
+  //colField.focus()
 
-  btn.onclick = () => {
-    const attribute = colField.value.trim();
-    const entryString = input.value;
+  const attribute = colField.value.trim();
+  const entryString = input.value;
 
-    let arr = entryString.split(/\r?\n/);
-    arr = arr.map((x) => x.trim());
-    arr = arr.filter((x) => x.length > 0);
+  let arr = entryString.split(/\r?\n/);
+  arr = arr.map((x) => x.trim());
+  arr = arr.filter((x) => x.length > 0);
 
-    const outputArr = [];
+  const outputArr = [];
 
-    for (const name of arr) {
+  for (const name of arr) {
 
-      let converted = `  <value>${name}</value>`;
-      outputArr.push(converted);
-    };
+    let converted = `  <value>${name}</value>`;
+    outputArr.push(converted);
+  };
 
-    //add condition filter
-    
-    outputArr.unshift(`<condition attribute="${attribute}" operator="in">`);
-    outputArr.push('</condition>')
+  //add condition filter
 
-    //add filter tags before and after
-    if (checkbox.checked) {
+  outputArr.unshift(`<condition attribute="${attribute}" operator="in">`);
+  outputArr.push('</condition>');
+
+  //add filter tags before and after
+  if (checkbox.checked) {
     outputArr.unshift('<filter type="or">');
-    outputArr.push('</filter>')
-    };
-    const outputString = outputArr.join('\n');
+    outputArr.push('</filter>');
+  };
 
-    output.value = outputString;
+  const outputString = outputArr.join('\n');
 
-  }
+  output.value = outputString;
+
 }
