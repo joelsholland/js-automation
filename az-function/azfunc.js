9cc7c5ef5bf879e871272a9583cc53cc9bb8d027
Joel Trepp <joeltrepp@hotmail.com>
2025-02-25 15:07:28 +0100

    add api-call

--- az-function/azfunc.js
+++ az-function/azfunc.js
@@ -4,6 +4,26 @@ ageVal = document.querySelector('.inputAge').value;
 btn = document.querySelector('button');
 btn.addEventListener("click", getAzFunc);
 
+async function call_api(){
+const baseUrl = "https://web-app-rooibos.azurewebsites.net/api/HttpTrigger_First"
+const fullUrl = `${baseUrl}?name=${nameVal}&age=${ageVal}}`; 
+
+try {
+    const response = await fetch(fullUrl);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 function getAzFunc() {
     console.log(`${nameVal} ${ageVal}`)
+    a = call_api() ;
+    console.log(a);
+
 }
